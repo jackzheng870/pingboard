aa5c125e0256bef9e745eceadf757c64dc88f5bb
Jack Zheng <jackzheng870@icloud.com>
2021-03-14 23:25:35 +0800

    Cancel old tasks before adding new ones

--- src/main/java/io/github/jackzheng870/pingboard/PingboardPlugin.java
+++ src/main/java/io/github/jackzheng870/pingboard/PingboardPlugin.java
@@ -1,9 +1,13 @@
 package io.github.jackzheng870.pingboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
@@ -11,6 +15,7 @@ import org.bukkit.scoreboard.Scoreboard;
 
 public class PingboardPlugin extends JavaPlugin {
     Scoreboard pingboard;
+    private List<BukkitTask> updatePingTasks = new ArrayList<BukkitTask>();
 
     @Override
     public void onEnable() {
@@ -29,15 +34,20 @@ public class PingboardPlugin extends JavaPlugin {
         Objective objective = scoreboard.registerNewObjective("pingboard", "dummy", "Pingboard");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
+        updatePingTasks.forEach(task -> task.cancel());
+        updatePingTasks.clear();
+
         for (Player player : Bukkit.getOnlinePlayers()) {
             Score score = objective.getScore(player.getName());
 
-            new BukkitRunnable() {
+            BukkitTask task = new BukkitRunnable() {
                 @Override
                 public void run() {
                     score.setScore(getPing(player));
                 }
             }.runTaskTimer(this, 0, 20);
+
+            updatePingTasks.add(task);
         }
         return scoreboard;
     }
